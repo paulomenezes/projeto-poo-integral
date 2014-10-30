33473007743b82d6dba19ac11095ad7fda080041
alessonrenato <alessonrenato@gmail.com>
2014-10-30 01:29:03 +0000

    adição das entidades DesafioAberto & DesafioMultiplaEscolha

---
+++ projeto_integralPOO/src/com/ufrpe/integrais/dados/entidades/DesafioAberto.java
@@ -0,0 +1,23 @@
+package com.ufrpe.integrais.dados.entidades;
+
+public class DesafioAberto extends Desafio {
+	private String solucao;
+	private String resposta;
+
+	public String getSolucao() {
+		return solucao;
+	}
+
+	public void setSolucao(String solucao) {
+		this.solucao = solucao;
+	}
+
+	public String getResposta() {
+		return resposta;
+	}
+
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
+
+}
