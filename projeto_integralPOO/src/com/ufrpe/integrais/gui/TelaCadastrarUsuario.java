1d40b6d1002d3bbd89400cc2c28a7a3293cd0572
guilhermemelo3451 <guilhermemelo3451@gmail.com>
2014-12-13 00:13:41 +0000

    Tela cadastro OK

--- projeto_integralPOO/src/com/ufrpe/integrais/gui/TelaCadastrarUsuario.java
+++ projeto_integralPOO/src/com/ufrpe/integrais/gui/TelaCadastrarUsuario.java
@@ -1,12 +1,16 @@
 package com.ufrpe.integrais.gui;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.text.ParseException;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JButton;
-import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -17,300 +21,275 @@ import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.text.MaskFormatter;
 
-import com.ufrpe.integrais.dados.entidades.Usuario;
 import com.ufrpe.integrais.dados.entidades.excesoes.ObjetoJaExistenteExcepitions;
+import com.ufrpe.integrais.negocio.CadastroUsuarios;
 import com.ufrpe.integrais.util.Constantes;
 import com.ufrpe.integrais.util.Funcoes;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
-public class TelaCadastrarUsuario extends Tela {
+public class TelaCadastrarUsuario extends Tela implements KeyListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textFieldNome;
-	private JPasswordField passwordField_1;
 	private JTextField textFieldEmail;
-	private JPasswordField passwordField;
 	private JTextField textFieldSobreNome;
 	private JTextField textFieldUniversidade;
 	private JTextField textFieldCurso;
-	private JFormattedTextField textdataNascimento;
 	private JPasswordField passwordField_senha;
 	private JPasswordField passwordField_repSenha;
+	private Map<JTextField, Boolean> camposPreenchidos =  new HashMap<>();
+	private JButton btnCriarConta;
 
 	/**
 	 * Create the frame.
 	 * 
 	 * 
 	 */
-	
-	
-	
-	public TelaCadastrarUsuario() {
+
+	public TelaCadastrarUsuario() {		
 		setTitle("Integrais - Cadastrar Usuário");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		setBounds(Funcoes.centroDaTela(Constantes.CADASTRO_WIDTH, Constantes.CADASTRO_HEIGHT));
+
+		setBounds(Funcoes.centroDaTela(Constantes.CADASTRO_WIDTH,
+				Constantes.CADASTRO_HEIGHT));
 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNome = new JLabel("Nome");
 		lblNome.setBounds(10, 84, 46, 14);
 		contentPane.add(lblNome);
-		
+
 		textFieldNome = new JTextField();
 		textFieldNome.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyTyped(KeyEvent event) {
-				
-				String caracteres="0987654321";
-				
-				if(caracteres.contains(event.getKeyChar()+"")){
-				
-				JOptionPane.showMessageDialog(null,"Só permitido letras !!");	
-				event.consume();
+
+				String caracteres = "0987654321";
+
+				if (caracteres.contains(event.getKeyChar() + "")) {
+
+					JOptionPane.showMessageDialog(null,
+							"Só permitido letras !!");
+					event.consume();
 				}
-				
+
 			}
 		});
-		
-		
+
 		textFieldNome.setColumns(10);
 		textFieldNome.setBounds(10, 100, 167, 25);
+		textFieldNome.addKeyListener(this);
 		contentPane.add(textFieldNome);
-		
+
 		JLabel lblEmail = new JLabel("E-mail");
 		lblEmail.setBounds(11, 134, 46, 14);
 		contentPane.add(lblEmail);
-		
+
 		JLabel lblCadastrarUsurio = new JLabel("Cadastrar Usu\u00E1rio");
 		lblCadastrarUsurio.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCadastrarUsurio.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		lblCadastrarUsurio.setBounds(10, 11, 374, 58);
 		contentPane.add(lblCadastrarUsurio);
-		
-		JButton btnCriarConta = new JButton("Criar conta");
+
+		btnCriarConta = new JButton("Criar conta");
+		btnCriarConta.setToolTipText("preencha todos os campos ");
+		btnCriarConta.setEnabled(false);
 		btnCriarConta.addActionListener(new ActionListener() {
-			
-			
-			
+
 			public void actionPerformed(ActionEvent e) {
 				
 				
+				if(Funcoes.validarEmail(textFieldEmail.getText())){
 				
-				if(! textFieldNome.getText().isEmpty() && ! textFieldSobreNome.getText().isEmpty() ){
+				if(new String(passwordField_senha.getPassword()).equals(new String(passwordField_repSenha.getPassword()))  ){
+				
 					
-					if( ! textFieldEmail.getText().isEmpty()){
-						
-						if(! passwordField_senha.getPassword().toString().isEmpty()){
-							
-							if(! passwordField_repSenha.getPassword().toString().isEmpty()  ){
-								
-								if(! textFieldUniversidade.getText().isEmpty()) {
-									
-									if(! textFieldCurso.getText().isEmpty()){
-										
-										if(! textdataNascimento.getText().isEmpty() ){
-											
-											
-								            if(Funcoes.validarEmail(textFieldEmail.getText())){
-								            	
-								            	
-								            	try {
-													
-													 Calendar c = Calendar.getInstance();
-													 Date data = c.getTime();
-													 
-													 Usuario u = new Usuario(11111,data ,textFieldNome.getText(),textFieldEmail.getText(), passwordField_senha.getPassword().toString(),
-																textFieldUniversidade.getText(),textFieldCurso.getText(), textdataNascimento.getText());
-													 
-													 fachada.cadastrarUsuario(u); 
-													 
-								            	}catch(ObjetoJaExistenteExcepitions e1 ){
-								            		
-								            		
-								            		JOptionPane.showMessageDialog(TelaCadastrarUsuario.this, Constantes.USUARIO_JA_EXISTE);
-								            	}
-								            	
-								           	
-								            	
-								            }else{
-								            	
-								            	JOptionPane.showMessageDialog(TelaCadastrarUsuario.this, Constantes.EMAIL_INVALIDO);
-								            }	
-											
-											
-											
-										}else{
-											JOptionPane.showMessageDialog(null,Constantes.DATANASCIMENTO_OBRIGATORIO);
-										}
-				
-										
-									}else{
-										JOptionPane.showConfirmDialog(null, Constantes.CURSO_OBRIGATORIO);
-									}
-									
-								}else{
-									JOptionPane.showMessageDialog(null, Constantes.UNIVERSIDADE_OBRIGATORIO);
-								}
-								
-							}else{
-								
-								JOptionPane.showMessageDialog(null, Constantes.REPETIRSENHA_OBRIGATORIO);
-							}
-							
-						}else{
-							
-							JOptionPane.showMessageDialog(null, Constantes.SENHA_OBRIGATORIO);
-						}
-						
-						
-					}else{
-						
-						
-						JOptionPane.showMessageDialog(null,Constantes.EMAIL_OBRIGATORIO);
-					}
 					
 					
 				}else{
 					
-					JOptionPane.showMessageDialog(null,Constantes.NOME_OBRIGATORIO);
+					
+					JOptionPane.showMessageDialog(null, Constantes.REPETIRSENHA_OBRIGATORIO);
 				}
 				
+				}else{
+					
+					JOptionPane.showMessageDialog(null,Constantes.EMAIL_INVALIDO);
+				}
+
 			}
-			
-			
-			
-			
+
 		});
-		
-		
-		
-		
+
 		btnCriarConta.setBounds(285, 325, 89, 25);
 		contentPane.add(btnCriarConta);
-		
-		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(394, 183, 190, 25);
-		contentPane.add(passwordField_1);
-		
+
+
 		JLabel lblSenha = new JLabel("Senha");
 		lblSenha.setBounds(394, 158, 46, 14);
 		contentPane.add(lblSenha);
-		
+
 		textFieldEmail = new JTextField();
 		textFieldEmail.setBounds(10, 153, 364, 25);
 		contentPane.add(textFieldEmail);
 		textFieldEmail.setColumns(10);
+		textFieldEmail.addKeyListener(this);
 		
 		JLabel lblUniversidade = new JLabel("Universidade");
 		lblUniversidade.setBounds(394, 211, 62, 14);
 		contentPane.add(lblUniversidade);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(394, 233, 190, 25);
-		contentPane.add(passwordField);
-		
+
+
 		textFieldSobreNome = new JTextField();
+		textFieldSobreNome.addKeyListener(this);
 		textFieldSobreNome.addKeyListener(new KeyAdapter() {
-		
+
 			@Override
 			public void keyTyped(KeyEvent e) {
-				
-				String caracteres="0987654321";
-				if(caracteres.contains(e.getKeyChar()+"")){
-					
-				JOptionPane.showMessageDialog(null, "Só permitido letras !!");	
-				e.consume();
+
+				String caracteres = "0987654321";
+				if (caracteres.contains(e.getKeyChar() + "")) {
+
+					JOptionPane.showMessageDialog(null,
+							"Só permitido letras !!");
+					e.consume();
 				}
-				
+
 			}
 		});
-		
-		
-		
+
 		textFieldSobreNome.setColumns(10);
 		textFieldSobreNome.setBounds(187, 100, 190, 25);
 		contentPane.add(textFieldSobreNome);
-		
+
 		JLabel lblSobrenome = new JLabel("Sobrenome");
 		lblSobrenome.setBounds(187, 84, 77, 14);
 		contentPane.add(lblSobrenome);
-		
+
 		JLabel lblSenha_1 = new JLabel("Senha");
 		lblSenha_1.setBounds(10, 184, 46, 14);
 		contentPane.add(lblSenha_1);
-		
+
 		JLabel lblRepitaASenha = new JLabel("Repita a senha");
 		lblRepitaASenha.setBounds(187, 184, 77, 14);
 		contentPane.add(lblRepitaASenha);
-		
+
 		JLabel lblUniversidade_1 = new JLabel("Universidade");
 		lblUniversidade_1.setBounds(10, 233, 77, 14);
 		contentPane.add(lblUniversidade_1);
-		
+
 		textFieldUniversidade = new JTextField();
 		textFieldUniversidade.setColumns(10);
 		textFieldUniversidade.setBounds(10, 249, 167, 25);
 		contentPane.add(textFieldUniversidade);
+		textFieldUniversidade.addKeyListener(this);
 		
 		textFieldCurso = new JTextField();
 		textFieldCurso.setColumns(10);
 		textFieldCurso.setBounds(187, 249, 190, 25);
 		contentPane.add(textFieldCurso);
-		
+		textFieldCurso.addKeyListener(this);
+
 		JLabel lblCurso = new JLabel("Curso");
 		lblCurso.setBounds(187, 233, 77, 14);
 		contentPane.add(lblCurso);
-		
+
 		JButton btnVoltas = new JButton("Voltar");
 		btnVoltas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				
+
 				Login login = new Login();
 				login.setVisible(true);
-				
+
 				TelaCadastrarUsuario.this.setVisible(false);
-				
-				
+
 			}
 		});
 		btnVoltas.setBounds(187, 326, 89, 25);
 		contentPane.add(btnVoltas);
-		
-		JLabel lblDataDeNascimento = new JLabel("Data de Nascimento");
-		lblDataDeNascimento.setBounds(10, 285, 138, 14);
-		contentPane.add(lblDataDeNascimento);
 
 		try {
-			MaskFormatter mf1 = new MaskFormatter("          #####/#####  /#####");
-		    mf1.setPlaceholderCharacter('_');
-			
-			textdataNascimento = new JFormattedTextField(mf1);
-			textdataNascimento.setBounds(10, 301, 167, 25);
-			
-			contentPane.add(textdataNascimento);
-			
-		
+			MaskFormatter mf1 = new MaskFormatter(
+					"          #####/#####  /#####");
+			mf1.setPlaceholderCharacter('_');
+
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		passwordField_senha = new JPasswordField();
 		passwordField_senha.setBounds(10, 206, 167, 25);
 		contentPane.add(passwordField_senha);
-		
+		passwordField_senha.addKeyListener(this);
+
 		passwordField_repSenha = new JPasswordField();
 		passwordField_repSenha.setBounds(187, 206, 187, 25);
 		contentPane.add(passwordField_repSenha);
+		passwordField_repSenha.addKeyListener(this);
+	}
+	
+	private void campoPreenchido(JTextField field) {
+		
+		
+		if (field.getText().length() > 0) {
+			camposPreenchidos.put(field, true);
+		} else {
+			camposPreenchidos.put(field, false);
+		}
+		
+		boolean resultado = true;
+		if (camposPreenchidos.size() >= 7) {
+			for (JTextField v : camposPreenchidos.keySet()) {
+				if (!camposPreenchidos.get(v)) {
+					resultado = false;
+				}
+			}
+		} else {
+			resultado = false;
+		}
+		
+		if (resultado) {
+			btnCriarConta.setEnabled(true);
+			btnCriarConta.setToolTipText("");
+		} else {
+			btnCriarConta.setEnabled(false);
+			btnCriarConta.setToolTipText("Preencha todos os campos");
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getSource().equals(textFieldNome)) {
+			campoPreenchido(textFieldNome);
+		} else if (e.getSource().equals(textFieldSobreNome)) {
+			campoPreenchido(textFieldSobreNome);
+		} else if (e.getSource().equals(textFieldEmail)) {
+			campoPreenchido(textFieldEmail);
+		} else if (e.getSource().equals(textFieldUniversidade)) {
+			campoPreenchido(textFieldUniversidade);
+		} else if (e.getSource().equals(textFieldCurso)) {
+			campoPreenchido(textFieldCurso);
+		} else if (e.getSource().equals(passwordField_senha)) {
+			campoPreenchido(passwordField_senha);
+		} else if (e.getSource().equals(passwordField_repSenha)) {
+			campoPreenchido(passwordField_repSenha);
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
